c7dfff777ec164a74ec605150b5c4a0b03af7523
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-17 11:32:03 +0530

    DualControls
    Control two spheres red and green one with WASD keys and andother with arrow keys

---
+++ Assets/DualControl/MoveScript1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript1 : MonoBehaviour
+{
+    [Range(0, 50)]
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("W"))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetButton("S"))
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetButton("A"))
+            transform.Translate( -1* speed * Time.deltaTime,0,0);
+        if (Input.GetButton("D"))
+            transform.Translate(1 * speed * Time.deltaTime,0,0);
+    }
+}
---
+++ Assets/DualControl/MoveScript2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript2 : MonoBehaviour
+{
+    [Range(0,50)]
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("UP"))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetButton("DOWN"))
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetButton("LEFT"))
+            transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+        if (Input.GetButton("RIGHT"))
+            transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+    }
+}
